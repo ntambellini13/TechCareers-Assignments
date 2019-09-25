95674116d1ad14b142d9a0315cbd6c9dfd78b74d
835266 <835266@ICT218460>
2019-09-25 15:12:19 -0600

    Added updated version of CPRG214 Lab 3 Assignment

--- ASP.NET/Nicolas_Tambellini_CPRG214_Lab_3/Nicolas_Tambellini_CPRG214_Lab_3/Controllers/ProductsController.cs
+++ ASP.NET/Nicolas_Tambellini_CPRG214_Lab_3/Nicolas_Tambellini_CPRG214_Lab_3/Controllers/ProductsController.cs
@@ -13,12 +13,21 @@ namespace Nicolas_Tambellini_CPRG214_Lab_3.Controllers
         /// Products Page Controller
         /// </summary>
         /// <returns>Products page with table of products from the DB</returns>
+        [HttpGet]
         public ActionResult Products()
         {
-            ViewBag.Title = "Products"; // changes title on tab to Products
-            List<Product> productList = ProductDB.GetProducts(); // list of products from the DB to populate to the view
 
-            return View(productList);
+            ViewBag.Title = "Products"; // changes title on tab to Products
+            try
+            {
+                List<Product> productList = ProductDB.GetProducts(); // list of products from the DB to populate to the view
+                return View(productList);
+            }
+            catch
+            {
+                return View(); // returns empty view
+            }
+            
         }
 
         /// <summary>
@@ -26,21 +35,31 @@ namespace Nicolas_Tambellini_CPRG214_Lab_3.Controllers
         /// </summary>
         /// <param name="id">id of product that was selected</param>
         /// <returns>Details of selected product including long description and image</returns>
+        [HttpGet]
         public ActionResult Details(string id)
         {
             ViewBag.Title = "Product Details"; // changes title on tab to Details
-            List<Product> productList = ProductDB.GetProducts(); // list of products from the DB
-            Product selectedProduct = new Product(); // selected product to display details of
 
-            // loop through products list to find id of selected product
-            foreach (Product p in productList)
+            try
             {
-                if (p.ProductID == id)
+                List<Product> productList = ProductDB.GetProducts(); // list of products from the DB
+                Product selectedProduct = new Product(); // selected product to display details of
+
+                // loop through products list to find id of selected product
+                foreach (Product p in productList)
                 {
-                    selectedProduct = p;
+                    if (p.ProductID == id)
+                    {
+                        selectedProduct = p;
+                    }
                 }
+                return View(selectedProduct);
             }
-            return View(selectedProduct);
+            catch
+            {
+                return View(); // returns empty view
+            }
+            
         }
     }
 }
--- ASP.NET/Nicolas_Tambellini_CPRG214_Lab_3/Nicolas_Tambellini_CPRG214_Lab_3/Models/Product.cs
+++ ASP.NET/Nicolas_Tambellini_CPRG214_Lab_3/Nicolas_Tambellini_CPRG214_Lab_3/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,33 @@ namespace Nicolas_Tambellini_CPRG214_Lab_3.Models
 {
     public class Product
     {
+        [Required(ErrorMessage = "A Product ID is required")]
+        [StringLength(10)]
         public string ProductID { get; set; } // Product ID
+
+        [Required(ErrorMessage = "A product name is required")]
+        [StringLength(50)]
         public string Name { get; set; } // Product Name
+
+        [Required(ErrorMessage = "A short product description of less than 200 characters is required")]
+        [StringLength(200)]
         public string ShortDescription { get; set; } // Short Description of Product
+
+        [Required(ErrorMessage = "A product description of less than 2000 characters is required")]
+        [StringLength(2000)]
         public string LongDescription { get; set; } // Long and detailed Description of Product
+
+        [Required(ErrorMessage = "A Category ID is required")]
+        [StringLength(10)]
         public string CategoryID { get; set; } // Product Category ID
+
+        [StringLength(30)]
         public string ImageFile { get; set; } // Image file path for product
+
+        [Required(ErrorMessage = "The product needs a price")]
         public decimal UnitPrice { get; set; } // Price of Product
+
+        [Required(ErrorMessage = "The quantity of the product is needed")]
         public int OnHand { get; set; } // Quantity of product available on hand
     }
 }
